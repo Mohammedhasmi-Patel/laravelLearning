7de41a8c203a2ca6a912e826d3e4e437bce2aec9
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-12-29 11:22:18 +0530

    learn basic of blade templete and understand condition structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,26 @@
 <h1>Learning Laravel</h1>
 
-<a href="{{route('about')}}">go to post</a>
+
+
+@php
+  $names = ["Salman Khan","Shahid kapoor","Katrina Kaif"]
+@endphp
+
+<ul>
+@foreach ($names as $name)
+  @if($loop->even)
+    <li style="color:red">{{$name}}</li>
+  @elseif($loop->odd)
+  <li style="color:green">{{$name}}</li>
+
+
+  @else
+  <li>{{$name}}</li>
+
+  @endif
+
+@endforeach
+</ul>
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,3 @@ Route::get('/', function () {
 
 });
 
-Route::prefix('post')->group(function () {
-    Route::get('admin', function () {
-        return view('post');
-    });
-
-    Route::get('user', function () {
-        return "<h1>i am user</h1>";
-    });
-
-    Route::get('teamlead', function () {
-        return "<h1>i am teamlead</h1>";
-    });
-
-});
-
-
-
